cc9358e3db15b9b22fe820658736fcb116026c0b
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-06 12:40:55 -0800

    we added the full article to website

--- ozge-jesus/scripts/article.js
+++ ozge-jesus/scripts/article.js
@@ -27,18 +27,16 @@ Article.prototype.toHtml = function() {
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
 
-      1. author name,
       2. author url,
-      3. article title,
-      4. article body, and
-      5. publication date. */
 
-    //$newArticle.attr('author' ,this.author);
-    $newArticle.find('byLine',this)
+  */
 
-
-
-
+  //$newArticle.attr('author' ,this.author);
+  //$newArticle.find('').append(`<a href="">$this.title`)
+  $newArticle.find('a').html(`${this.author}`);
+  $newArticle.find('h1').html(`${this.title}`);
+  $newArticle.find('.article-body').html(`${this.body}`);
+  $newArticle.find('a').attr('href', `${this.authorUrl}`);
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
