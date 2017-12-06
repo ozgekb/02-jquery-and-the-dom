a3eac334bbef9a75ecb7c07122478e9dd10c5144
sundownspecial <je.mendoza@live.com>
2017-12-06 13:44:52 -0800

    completed readme and styled css

--- ozge-jesus/scripts/article.js
+++ ozge-jesus/scripts/article.js
@@ -16,39 +16,25 @@ function Article (rawData) {
 }
 
 Article.prototype.toHtml = function() {
-  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  //It makes a full copy of the articles and all of its descendent elements.
-
   let $newArticle = $('article.template').clone();
   $newArticle.removeClass('template');
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
-    We need to fill in:
-
-      2. author url,
-
-  */
-
-  //$newArticle.attr('author' ,this.author);
-  //$newArticle.find('').append(`<a href="">$this.title`)
   $newArticle.find('a').html(`${this.author}`);
   $newArticle.find('h1').html(`${this.title}`);
   $newArticle.find('.article-body').html(`${this.body}`);
   $newArticle.find('a').attr('href', `${this.authorUrl}`);
-  // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
   return $newArticle;
 };
 
 rawData.sort(function(a,b) {
-  // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-// TODO: Refactor these for loops using the .forEach() array method.
+
 
 rawData.forEach(function(rawData) {
   articles.push(new Article(rawData));
@@ -57,8 +43,3 @@ rawData.forEach(function(rawData) {
 articles.forEach(function(articles){
   $('#articles').append(articles.toHtml());
 });
-
-
-// for(let i = 0; i < articles.length; i++) {
-//   $('#articles').append(articles[i].toHtml());
-// }
