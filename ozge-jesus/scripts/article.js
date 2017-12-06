44de3550f91e87d456596107a7347d552e571f05
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-06 13:11:21 -0800

    we did first forech

--- ozge-jesus/scripts/article.js
+++ ozge-jesus/scripts/article.js
@@ -50,9 +50,9 @@ rawData.sort(function(a,b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+rawData.forEach(function(rawData) {
+  articles.push(new Article(rawData));
+});
 
 for(let i = 0; i < articles.length; i++) {
   $('#articles').append(articles[i].toHtml());
